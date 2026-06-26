517a4cb9d930a7862d291ae14b43a43c9b7d88cc
bmarques1995 <brunosmarques1995@hotmail.com>
2026-06-26 14:01:43 -0300

    added first lib with vcpkg

--- src/sample.cpp
+++ src/sample.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <spdlog/spdlog.h>
 
 int main(int argc, char** argv)
 {
     std::cout << "Hello Cmake\n";
+    spdlog::warn("Easy padding in numbers like {:08d}", 12);
     return 0;
 }
