f003595f44069add3b26bf554ecd4f9015521b48
bmarques1995 <brunosmarques1995@hotmail.com>
2026-06-28 11:53:10 -0300

    Crow sample server

--- src/sample.cpp
+++ src/sample.cpp
@@ -1,9 +1,19 @@
+#include <crow/common.h>
 #include <iostream>
 #include <spdlog/spdlog.h>
+#include <crow.h>
 
 int main(int argc, char** argv)
 {
     std::cout << "Hello Cmake\n";
     spdlog::warn("Easy padding in numbers like {:08d}", 12);
+    
+    crow::SimpleApp app;
+    CROW_ROUTE(app, "/").methods(crow::HTTPMethod::GET)
+    ([]{
+        crow::json::wvalue x({{"message", "Hello, World!"}});
+        x["message2"] = "Hello, World.. Again!";
+        return x;
+    });
     return 0;
 }
